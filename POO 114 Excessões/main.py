616cb33ebc770d6440b79167e0ecd458604e5263
Hyago Andrade <hyagosdj@gmail.com>
2024-03-31 14:07:33 -0300

    Atualização de exercicios: Excessoes e Dunder Methods

--- POO 114 Excessões/main.py
+++ POO 114 Excessões/main.py
@@ -1,10 +1,25 @@
 class TaErradoError(Exception):
-    pass
+    ...
+
+class OutroError(Exception):
+    ...
+
 
 def testar():
-    raise TaErradoError('Errado!')
+    excessao = TaErradoError('A', 'B', 'C')
+    excessao.add_note('Olha a nota 1')
+    excessao.add_note('Você errou isso')
+    raise excessao
 
 try:
+    #1/0
     testar()
-except TaErradoError as error:
-    print(f'Erro: {error}')
+except (TaErradoError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    #print(f'Erro: {error}')
+    excessao = OutroError('Vou lançar novamente')
+    excessao.__notes__ = error.__notes__.copy()
+    excessao.add_note('Mais uma nota')
+    raise excessao from error
