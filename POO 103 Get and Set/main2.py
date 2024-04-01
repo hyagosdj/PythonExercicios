0ccc84023910ede9e5eca3f7b106fad68daa751a
Hyago Andrade <hyagosdj@gmail.com>
2024-04-01 19:28:29 -0300

    Atualização aulas e adição de conteúdo

---
+++ POO 103 Get and Set/main2.py
@@ -0,0 +1,44 @@
+# @property + @setter - getter e setter no modo Pythônico
+# - com getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Atributos que começar com um ou dois underlines não devem ser usados fora da classe.
+
+class Caneta:
+    def __init__(self, cor):
+        #self.cor_tinta = cor
+        # private protected
+        self._cor = cor # Esse atributo é para o @property(getter)
+        self._cor_tampa = None # Nesse caso não tem no init, logo ele deve ser deixado assim. 
+
+    @property
+    def cor(self):
+        return self._cor
+    
+    @cor.setter # É um método que tem self, tem que receber um valor 
+    def cor(self, valor):
+        if valor == 'Rosa':
+            raise ValueError('Não aceito essa cor')
+        # print('Estou no SETTER', valor)
+        self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+    
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+    
+# def mostrar(caneta):
+#     return caneta.cor
+
+
+caneta = Caneta('Azul')
+caneta.cor = 'Pink' # Error de atribuição | PARA CASOS EM QUE NÃO SE USOU O GETTER/SETTER
+caneta.cor_tampa = 'Azul'
+# getter -> obter valor
+print(caneta.cor)
+print(caneta.cor_tampa)
+# mostrar(caneta)
--- POO 116 Context Manager/main.py
+++ POO 116 Context Manager/main.py
@@ -1,26 +1,63 @@
 
 # Deste jeito é necessário o CLOSE()
+# Por extenso:
 #arquivo = open('abc.txt', 'w')
 #arquivo.write('Alguma coisa')
 #arquivo.close()
 
+# Abreviado:
 # Desta forma, não é necessário.
 #with open('abc.txt', 'w') as arquivo:
 #    arquivo.write('Alguma coisa')
 
 
-class Arquivo:
-    def __init__(self, arquivo, modo):
-        print('INIT')
-        self.arquivo = open(arquivo, modo)
+# class Arquivo:
+#     def __init__(self, arquivo, modo):
+#         print('INIT')
+#         self.arquivo = open(arquivo, modo)
 
-    def __enter__(self):
-        print('Entrou na classe')
-        return self.arquivo
+#     def __enter__(self):
+#         print('Entrou na classe')
+#         return self.arquivo
 
-    def __exit__(self):
-        print('Saiu da CLASSE')
-        self.arquivo.close()
+#     def __exit__(self):
+#         print('Saiu da CLASSE')
+#         self.arquivo.close()
 
 """ with open('abc.txt', 'w') as arquivo:
     arquivo.write('Alguma coisa') """
+
+class MyOpen:
+    def __init__(self, caminho_arquivo, modo):
+        self.caminho_arquivo = caminho_arquivo
+        self.modo = modo
+        self._arquivo = None
+
+    def __enter__(self):
+        print('ABRINDO ARQUIVO')
+        self._arquivo = open(self.caminho_arquivo, self.modo, encoding='utf8')
+        return self._arquivo
+
+    def __exit__(self, class_exception, exception_, traceback_):
+        print('FECHANDO ARQUIVO')
+        self._arquivo.close()
+
+        # raise class_exception(*exception_.args).with_traceback(traceback_)
+
+        # print(class_exception)
+        # print(exception_)
+        # print(traceback_)
+        
+        exception_.add_note('Minha nota.')
+
+        # raise ConnectionError('Não deu para conectar.')
+
+        # return True # Tratei a exceção
+
+# instancia = MyOpen('aulaContexManager.txt', 'w')
+
+with MyOpen('aulaContexManager.txt', 'w') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n', 123)
+    arquivo.write('Linha 3\n')
+    print('WITH', arquivo)
