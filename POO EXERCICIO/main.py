02c0a4698af14e7eb5229d68d7eb09a00b8633e8
Hyago Andrade <hyagosdj@gmail.com>
2024-04-03 21:25:41 -0300

    Atualizacao de Desafio

--- POO EXERCICIO/main.py
+++ POO EXERCICIO/main.py
@@ -1,6 +1,33 @@
-from ct_corre import ContaCorrente
-from ct_poupa import ContaPoupanca
+"""
+Conta(ABC)
+    ContaCorrente
+    ContaPoupanca
 
-ct_corre = ContaCorrente(agencia=1010, conta=0000, saldo=0, limite=1000)
-ct_corre.depositar(800)
-ct_corre.sacar('1234')
+Pessoa(ABC)
+    Cliente
+        Cliente -> Conta
+
+Banco
+    Banco -> Cliente
+    Banco -> Conta
+
+
+Dicas:
+Criar classe Cliente que herda da classe Pessoa (Herança)
+    Pessoa tem nome e idade (com getter)
+    Cliente TEM conta (Agregação da classe ContaCorrente ou ContaPoupanca)
+Criar classes ContaCorrente e ContaPoupanca que herdam de Conta
+    ContaCorrente deve ter um limite extra
+    Contas têm agência, número da conta e saldo
+    Contas devem ter método para depósito
+    Conta (super classe) deve ter o método abstrato (Abstração e 
+    polimorfismo - as subclasses que implementam o método sacar)
+Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
+Banco será responsável autenticar o cliente e as contas da seguinte maneira:
+    Banco tem contas e clientes (Agregação)
+    * Checar se a agência é daquele banco
+    * Checar se a cliente é daquele banco
+    * Checar se a conta é daquele banco
+Só será possível sacar se passar na autenticação do banco (descrita acima)
+Banco autentica por um método.
+"""
