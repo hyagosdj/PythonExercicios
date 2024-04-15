cf288cd957af9f06a74dd0a6e95cbc3f09b04ec1
Hyago Andrade <hyagosdj@gmail.com>
2024-04-14 23:57:49 -0300

    atualizandoooo

--- MODULOS_Threads/meusthreads.py
+++ MODULOS_Threads/meusthreads.py
@@ -1,7 +1,7 @@
-from threading import Thread
+from threading import Lock, Thread
 from time import sleep
 
-
+"""
 class MeuThread(Thread):
     def __init__(self, texto, tempo):
         self.texto = texto
@@ -26,3 +26,75 @@ t3.start()
 for i in range(20):
     print(i)
     sleep(1)
+"""
+
+"""
+
+
+def vai_demorar(texto, tempo):
+    sleep(tempo)
+    print(texto)
+
+
+t = Thread(target=vai_demorar, args=('Olá mundo!', 5))
+t.start()
+
+t1 = Thread(target=vai_demorar, args=('Olá mundo!', 1))
+t1.start()
+
+t2 = Thread(target=vai_demorar, args=('Olá mundo!', 2))
+t2.start()
+
+for i in range(20):
+    print(i)
+    sleep(.5)
+"""
+
+"""
+
+
+def vai_demorar(texto, tempo):
+    sleep(tempo)
+    print(texto)
+
+
+t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 10))
+t1.start()
+t1.join()
+"""  # while t1.is_alive():
+# print('Esperando a thread.')
+# sleep(2)
+
+# print('Thread acabou!')
+
+
+class Ingressos:
+    def __init__(self, estoque):
+        self.estoque = estoque
+        self.lock = Lock()
+
+    def comprar(self, quantidade):
+        self.lock.acquire()
+
+        if self.estoque < quantidade:
+            print('Não temos ingressos suficientes.')
+            self.lock.release()
+            return
+
+        sleep(1)
+
+        self.estoque -= quantidade
+        print(f'Você comprou {quantidade} ingresso(s). '
+              f'Ainda temos {self.estoque} em estoque')
+
+        self.lock.release()
+
+
+if __name__ == '__main__':
+    ingressos = Ingressos(10)
+
+    for i in range(1, 20):
+        t = Thread(target=ingressos.comprar, args=(i,))
+        t.start()
+        # ingressos.comprar(i)
+    print(ingressos.estoque)
